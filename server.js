73a4bc63bdb3fb90e1111743fe3fdc07c564145a
Can Yücel Yavuz <101628143+cyucelyavuz@users.noreply.github.com>
2022-04-10 21:57:23 +0300

    emit 21:58

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const session = require('express-session');
 const passport = require('passport');
 const routes = require('./routes');
 const auth = require('./auth.js');
+
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 
 app.set('view engine', 'pug');
 
@@ -19,15 +19,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-
-
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -35,18 +26,21 @@ app.use(session({
   cookie: { secure: false },
 }));
 
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
-  io.on('connection', socket => {
-    console.log('A user has connected');
-  });
+
   routes(app, myDataBase);
   auth(app, myDataBase);
+
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user count', currentUsers);
+    console.log('A user has connected');
+  });
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
