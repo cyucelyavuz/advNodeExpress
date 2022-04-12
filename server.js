64c84a5b96ac31e84955aecad1c3565fbb125f2e
Can Yücel Yavuz <101628143+cyucelyavuz@users.noreply.github.com>
2022-04-12 12:24:01 +0300

    messaging interface

--- server.js
+++ server.js
@@ -61,6 +61,10 @@ myDB(async (client) => {
       currentUsers,
       connected: true
     });
+
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { name: socket.request.user.name, message });
+    });
     console.log('A user has connected');
 
     socket.on('disconnect', () => {
