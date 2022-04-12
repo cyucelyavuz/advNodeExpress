42e45816dee385bccf30cf48ce959580999c851a
Can Yücel Yavuz <101628143+cyucelyavuz@users.noreply.github.com>
2022-04-12 11:40:49 +0300

    socket session

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const passportSocketIo= require("passport.socketio");
+const MongoStore = require("connect-mongo")(session);
+const store = new MongoStore({url:process.env.MONGO_URI})
+const cookieParser = require("cookie-parser");
+
 app.set('view engine', 'pug');
 
 fccTesting(app); // For fCC testing purposes
@@ -24,10 +29,23 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false },
+  key: 'express.sid',
+  store:store
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
@@ -50,6 +68,18 @@ myDB(async (client) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 http.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
